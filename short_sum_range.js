b6b85d4ea2a573d8fdc0af0791032f57173262ca
django0212 <74166842+django0212@users.noreply.github.com>
2021-04-13 16:35:13 +0530

    Create short_sum_range.js

---
+++ short_sum_range.js
@@ -0,0 +1,13 @@
+  console.clear()
+// only change code below this line
+
+function sum(a, b) {
+	count = 0;
+	for (var i = a; i < b+1; i++) {
+		count += 1;
+	}
+	sum = ((a+b)/2)*count;
+	console.log(sum)
+}
+
+sum(1, 10);
