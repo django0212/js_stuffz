4934b624814ff19742c87a238f97a0ea27a1d1ed
django0212 <74166842+django0212@users.noreply.github.com>
2021-04-16 09:05:22 +0530

    Create base_calc.js

---
+++ base_calc.js
@@ -0,0 +1,134 @@
+console.clear()
+// only change code below this line
+// a = num1, b = num2, c = base
+
+function con(a, b) {
+	var dic = [];
+	while (a >= 1) {
+		dic.push(Math.floor(a%b));
+		a/=b;
+	}
+	return dic.reverse().join("");
+}
+
+function add(a, b, c) {
+	var n1 = Array.from(String(a), Number);
+	var n2 = Array.from(String(b), Number);
+	var num1 = 0;
+	var num2 = 0;
+
+	if (n1.some(el => el > c-1) || n2.some(el => el > c-1) ) {
+		console.log(`Input is not base ${c} compliant.`);
+	} else {
+		for (var i = 0; i < n1.length; i++) {
+		num1 += (n1[i]) * (c**((n1.length-1) - i));
+		};
+		for (var i = 0; i < n2.length; i++) {
+			num2 += (n2[i]) * (c**((n2.length-1) - i));
+		};
+
+		var sum = num1+num2
+		var conSum = con(sum, c)
+
+		console.log(`Base 10: ${sum}`);
+		console.log(`Base ${c}: ${conSum}`);
+	}
+}
+
+function add(a, b, c) {
+	var n1 = Array.from(String(a), Number);
+	var n2 = Array.from(String(b), Number);
+	var num1 = 0;
+	var num2 = 0;
+
+	if (n1.some(el => el > c-1) || n2.some(el => el > c-1) ) {
+		console.log(`Input is not base ${c} compliant.`);
+	} else {
+		for (var i = 0; i < n1.length; i++) {
+		num1 += (n1[i]) * (c**((n1.length-1) - i));
+		};
+		for (var i = 0; i < n2.length; i++) {
+			num2 += (n2[i]) * (c**((n2.length-1) - i));
+		};
+
+		var sum = num1+num2
+		var conSum = con(sum, c)
+
+		console.log(`Base 10: ${sum}`);
+		console.log(`Base ${c}: ${conSum}`);
+	}
+}
+
+function sub(a, b, c) {
+	var n1 = Array.from(String(a), Number);
+	var n2 = Array.from(String(b), Number);
+	var num1 = 0;
+	var num2 = 0;
+
+	if (n1.some(el => el > c-1) || n2.some(el => el > c-1) ) {
+		console.log(`Input is not base ${c} compliant.`);
+	} else {
+		for (var i = 0; i < n1.length; i++) {
+		num1 += (n1[i]) * (c**((n1.length-1) - i));
+		};
+		for (var i = 0; i < n2.length; i++) {
+			num2 += (n2[i]) * (c**((n2.length-1) - i));
+		};
+
+		var sum = num1-num2
+		var conSum = con(sum, c)
+
+		console.log(`Base 10: ${sum}`);
+		console.log(`Base ${c}: ${conSum}`);
+	}
+}
+
+function mult(a, b, c) {
+	var n1 = Array.from(String(a), Number);
+	var n2 = Array.from(String(b), Number);
+	var num1 = 0;
+	var num2 = 0;
+
+	if (n1.some(el => el > c-1) || n2.some(el => el > c-1) ) {
+		console.log(`Input is not base ${c} compliant.`);
+	} else {
+		for (var i = 0; i < n1.length; i++) {
+		num1 += (n1[i]) * (c**((n1.length-1) - i));
+		};
+		for (var i = 0; i < n2.length; i++) {
+			num2 += (n2[i]) * (c**((n2.length-1) - i));
+		};
+
+		var sum = num1*num2
+		var conSum = con(sum, c)
+
+		console.log(`Base 10: ${sum}`);
+		console.log(`Base ${c}: ${conSum}`);
+	}
+}
+
+function div(a, b, c) {
+	var n1 = Array.from(String(a), Number);
+	var n2 = Array.from(String(b), Number);
+	var num1 = 0;
+	var num2 = 0;
+
+	if (n1.some(el => el > c-1) || n2.some(el => el > c-1) ) {
+		console.log(`Input is not base ${c} compliant.`);
+	} else {
+		for (var i = 0; i < n1.length; i++) {
+		num1 += (n1[i]) * (c**((n1.length-1) - i));
+		};
+		for (var i = 0; i < n2.length; i++) {
+			num2 += (n2[i]) * (c**((n2.length-1) - i));
+		};
+
+		var sum = num1/num2
+		var conSum = con(sum, c)
+
+		console.log(`Base 10: ${sum}`);
+		console.log(`Base ${c}: ${conSum}`);
+	}
+}
+
+div(4041, 11, 6);
